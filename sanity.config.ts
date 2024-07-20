aa7a426faa2858e8e70d87db51f8400f23d1ae45
Parth Marfatia <parthmarfatia153@gmail.com>
2024-07-20 18:35:23 +0530

    sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { codeInput } from "@sanity/code-input";
 import { table } from "@sanity/table";
-import { projectId, dataset } from "./lib/env.api";
+import {structureTool} from "sanity/structure";
 
 export default defineConfig({
-  name: "victoreke",
-  title: "victoreke.com",
+  name: "parthmarfatia",
+  title: "parthmarfatia",
   basePath: "/studio",
-  projectId,
-  dataset,
-  plugins: [deskTool(), visionTool(), codeInput(), table()],
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
+  plugins: [structureTool(), visionTool(), codeInput(), table()],
   schema: { types: schemaTypes },
 });
