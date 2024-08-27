d9d9650970172cbecbbd5742cea6f392089e1753
Parth Marfatia <parthmarfatia153@gmail.com>
2024-08-28 03:18:02 +0530

    adding links in hero and favicon fix

--- app/data/social.ts
+++ app/data/social.ts
@@ -2,6 +2,8 @@ import {
   BiLogoGithub,
   BiLogoLinkedinSquare,
   BiLogoGmail,
+  BiLogoDiscord,
+  BiLogoWhatsapp,
 } from "react-icons/bi";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
@@ -32,6 +34,20 @@ export const socialLinks = [
     name: "Gmail",
     url: "parthmarfatia789@gmail.com",
     icon: BiLogoGmail,
+    status: "social",
+  },
+  {
+    id: 5,
+    name: "Discord",
+    url: "https://discord.com/channels/@me",
+    icon: BiLogoDiscord,
+    status: "social",
+  },
+  {
+    id: 6,
+    name: "WhatsApp",
+    url: "https://wa.me/918879101505",
+    icon: BiLogoWhatsapp,
     status: "social",
   },
 ];
