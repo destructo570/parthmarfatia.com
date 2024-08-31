752781f96540ac48ae38816cd671ce801c8aceeb
Parth Marfatia <parthmarfatia153@gmail.com>
2024-08-31 12:48:34 +0530

    bucklist section

--- app/components/pages/Heroes.tsx
+++ app/components/pages/Heroes.tsx
@@ -14,20 +14,9 @@ export default async function Heroes() {
   return (
     <section className="mt-32 max-w-5xl">
       <Slide delay={0.17}>
-        <h2 className="text-4xl mb-4 font-bold tracking-tight">Heroes</h2>
+        <h2 className="text-4xl mb-4 font-bold tracking-tight">Bucket List</h2>
         <p className="dark:text-zinc-400 text-zinc-600 max-w-2xl">
-          Inspired by{" "}
-          <RefLink
-            href="https://rafa.design"
-            className="dark:text-blue-400 text-blue-500 underline"
-          >
-            Rafael Condo&apos;s
-          </RefLink>{" "}
-          heroes list, here&apos;s my own curated lineup of code conjurers and
-          digital dynamos that I&apos;m absolutely stoked to meet someday.{" "}
-          <strong className="font-semibold">
-            &quot;In no particular order&quot;
-          </strong>
+        A bucket list of destinations to explore, races to conquer, and moments to experience.
         </p>
       </Slide>
 
